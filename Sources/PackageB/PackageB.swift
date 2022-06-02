de7c9564ae8e9b3ea358cc97f475cbe37a0fbba3
Sreekanth Reddy <sreekanth.reddy@ymedialabs.com>
2022-06-02 17:47:08 +0530

    Updated

--- Sources/PackageB/PackageB.swift
+++ Sources/PackageB/PackageB.swift
@@ -1,7 +1,7 @@
 import PackageA
 
 public struct PackageB {
-    public private(set) var text = "Hello, World! 5:44"
+    public private(set) var text = "Hello, World! 5:46"
 
     public init() {
     }
