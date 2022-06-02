6f71764c16bbbac97815f36785e0f89ceab200d6
Sreekanth Reddy <sreekanth.reddy@ymedialabs.com>
2022-06-02 16:51:31 +0530

    Updated

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-         .package(url: "https://github.com/YMLSreekanth/PackageA.git", branch: "main")
+         .package(url: "https://github.com/YMLSreekanth/PackageA.git", from: "0.0.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
