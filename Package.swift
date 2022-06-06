2a72b7020e1a5caf425a6c2e141892c971f5f8a0
YMLSreekanth <102032533+YMLSreekanth@users.noreply.github.com>
2022-06-06 17:07:19 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let package = Package(
+       let package = Package(
     name: "PackageB",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/YMLSreekanth/PackageA.git", from: "0.0.1"),
+        .package(url: "https://github.com/YMLSreekanth/PackageA.git", from: "0.0.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
