cfee52d1bd0b6a61a00478112afe645199abc49b
YMLSreekanth <YMLSreekanth@users.noreply.github.com>
2022-06-07 11:07:28 +0000

    Swift format

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-                    let package = Package(
+let package = Package(
     name: "PackageB",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
