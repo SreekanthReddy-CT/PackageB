cb1f0a355e781c63672d9972fec0510c55b723ce
runner <runner@Mac-1691053301227.local>
2023-08-03 09:02:43 +0000

    Auto commit

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
          .package(
              url: "https://github.com/YMLSreekanth/PackageA.git", 
-             exact: "0.0.9")
+             exact: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
